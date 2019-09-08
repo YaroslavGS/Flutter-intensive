bca7711f8f2f24ae57cef0e4e3f0865118415f84
Yaroslav Gavrilov <yaroslav32123@gmail.com>
2019-09-08 18:39:37 +0300

    t

--- Lesson0/lesson0_1/test/lesson0_1_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:lesson0_1/lesson0_1.dart';
-// import 'package:test/test.dart';
-
-// // void main() {
-// //   test('calculate', () {
-// //     expect(calculate(), "Hello World!");
-// //   });
-// }
--- Lesson0/lesson0_2/test/lesson0_2_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:lesson0_2/lesson0_2.dart';
-// import 'package:test/test.dart';
-
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
